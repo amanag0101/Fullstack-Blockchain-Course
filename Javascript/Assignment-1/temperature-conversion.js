aad42a488e10488b9b9ba347501cf339d01c3dc8
amanag0101 <55055593+amanag0101@users.noreply.github.com>
2022-08-19 16:10:31 +0530

    Update temperature-conversion.js

--- Javascript/Assignment-1/temperature-conversion.js
+++ Javascript/Assignment-1/temperature-conversion.js
@@ -1,24 +1,23 @@
 // Program to convert temperatures to Fahrenheit and Celsius.
 
 // Function to convert temperature from Fahrenheit to Celsius.
-const convertFahrenheitToCelcius = (temperature) => {
+const convertFahrenheitToCelsius = (temperature) => {
     return (temperature - 32) / 1.8;
 }
 
 // Function to convert temperature from Celsius to Fahrenheit.
-const convertCelciusToFahrenheit = (temperature) => {
+const convertCelsiusToFahrenheit = (temperature) => {
     return (temperature * 1.8) + 32;
 }
 
 // Main
 const fahrenheit_inputs = [77, 86, 91.4, 95, 104];
-const celcius_inputs = [25, 30, 33, 35, 40];
+const celsius_inputs = [25, 30, 33, 35, 40];
 
 console.log("\nConverting Fahrenheit to Celsius");
 fahrenheit_inputs.forEach(temperature => 
-    console.log(`${temperature} F = ${convertFahrenheitToCelcius(temperature)} C`));
+    console.log(`${temperature} F = ${convertFahrenheitToCelsius(temperature)} C`));
 
 console.log("\nConverting Celsius to Fahrenheit");
-celcius_inputs.forEach(temperature => 
-    console.log(`${temperature} C = ${convertCelciusToFahrenheit(temperature)} F`));
-
+celsius_inputs.forEach(temperature => 
+    console.log(`${temperature} C = ${convertCelsiusToFahrenheit(temperature)} F`));
